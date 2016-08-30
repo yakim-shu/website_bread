fd18a1dfaae9ba195e1f4a8897aba4d2f3955e12
yakim <toromilk80129@gmail.com>
2016-08-30 18:27:47 +0800

    正式 start
    Signed-off-by: yakim <toromilk80129@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,19 @@ var gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
 	  livereload = require('gulp-livereload'),
 	  connect = require('gulp-connect'),
-    sourcemaps = require('gulp-sourcemaps');
+    sourcemaps = require('gulp-sourcemaps'),
+    jade = require('gulp-jade');
 
 
-var sourcePath = 'src/sass/*.scss', //原檔案路徑
-    targetPath = 'dist/css';         //編譯後路徑
+
+// src -  原檔案路徑
+// dist - 編譯後路徑
+var sourceCss  = 'src/sass/*.scss',
+    targetCss  = 'dist/css',
+    sourceJade = 'src/jade/**/*.jade',
+    targetJade = 'src/jade/dist',
+    sourceHtml = 'src/jade/dist/*.{htm,html}',
+    targetHtml = 'dist';
 
 
 /*=========================任務區============================*/
@@ -19,29 +27,40 @@ gulp.task('connect', function() {
   });
 });
 
+// jade===============
+gulp.task('jade', function(){
+  gulp.src(sourceJade)
+    .pipe(jade())
+    .pipe(jade({
+        pretty: true
+    }))
+    .pipe(gulp.dest(targetJade));
+});
 
 //編譯css(sass,autoprefixer補前綴)===============
 gulp.task('styles', function() {
-    gulp.src(sourcePath)
+    gulp.src(sourceCss)
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(sourcemaps.write())
         .pipe( autoprefixer( 'last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4' ) )
-        .pipe(gulp.dest(targetPath))
+        .pipe(gulp.dest(targetCss))
         .pipe(connect.reload());
 });
 
 //編譯html===============
 gulp.task('html', function () {
-  gulp.src("*.htm")
+  gulp.src(sourceHtml)
+    .pipe(gulp.dest(targetHtml))
     .pipe(connect.reload());
 });
 
 //監測變化===============
 gulp.task('watch', function () {
-  gulp.watch(sourcePath,['styles']);
-  gulp.watch("*.htm", ['html']);
+  gulp.watch(sourceCss,['styles']);
+  gulp.watch(sourceHtml, ['html']);
+  gulp.watch(sourceJade, ['jade']);
 });
 
 //預設執行任務===============
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', ['jade', 'watch', 'connect']);
