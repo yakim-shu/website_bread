f4e2eb7b41a42e28c8d6501ce9585db8b8e4ace6
yakim <toromilk80129@gmail.com>
2016-08-30 18:27:47 +0800

    正式 start
    Signed-off-by: yakim <toromilk80129@gmail.com>

---
+++ dist/js/common.js
@@ -0,0 +1,20 @@
+$(function() {
+	console.log("d");
+	// $('.gallery_1').imagesLoaded(function() {
+	    $('.gallery_1').vegas({
+	        slides: [
+	            { src: 'images/pic_pc_1.png' },
+	            { src: 'images/pic_a_2.jpg' },
+	            { src: 'images/pic_a_3.jpg' }
+	        ],
+
+	        align: "center",
+	        valign: "center",
+	        cover: "true",
+	        preload: "true",
+	        overlay: '../lib/jquery.vegas/overlays/05.png'
+
+	    });
+    // });
+
+});
